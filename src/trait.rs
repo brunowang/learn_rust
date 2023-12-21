be66ad598144aafedd97d5da996953d89b94a7aa
brunowang <625653507@qq.com>
2023-12-21 09:39:51 +0800

    testing impl trait for base type(extend trait) && check method and function

--- src/trait.rs
+++ src/trait.rs
@@ -7,6 +7,15 @@ fn main() {
     let c = Box::new(c) as Box<dyn Round>;
     println!("{}", c);
     println!("The area is {}", c.area());
+    let x : i32 = 10.double();
+    println!("{}", x);
+    let me = Chef;
+    <dyn Cook>::start(&me);
+    <Chef as Wash>::start(&me);
+
+    check_type(T::get1);
+    check_type(T::get2);
+    check_type(get3);
 }
 
 trait Shape {
@@ -39,3 +48,48 @@ impl Display for dyn Round {
         write!(f, "{{ radius:{} }}", self.get_radius())
     }
 }
+
+trait Double {
+    fn double(&self) -> Self;
+}
+
+impl Double for i32 {
+    fn double(&self) -> i32 {
+        *self * 2
+    }
+}
+
+// Fully Qualified Syntax. <T as TraitName>::item
+trait Cook {
+    fn start(&self);
+}
+
+trait Wash {
+    fn start(&self);
+}
+
+struct Chef;
+
+impl Cook for Chef {
+    fn start(&self) {
+        println!("Cook::start");
+    }
+}
+
+impl Wash for Chef {
+    fn start(&self) {
+        println!("Wash::start");
+    }
+}
+
+// There is no difference between methods and functions
+struct T(usize);
+
+impl T {
+    fn get1(&self) -> usize {self.0}
+    fn get2(&self) -> usize {self.0}
+}
+
+fn get3(t: &T) -> usize {t.0}
+
+fn check_type(_: fn(&T) -> usize) {}
