e8ba73f85e2885e0e23d67d2ed4d36e31aaac0fc
brunowang <625653507@qq.com>
2023-12-26 09:41:15 +0800

    testing '@' operator with reading issue: https://github.com/rust-lang/rust/issues/37854

--- src/destructure.rs
+++ src/destructure.rs
@@ -4,11 +4,14 @@ fn main() {
 
     let y = Some(Some(100));
     println!("{:?}", deep_match(y));
+
+    let y = Some(Some(105));
+    println!("{:?}", deep_match(y));
 }
 
 fn deep_match(v: Option<Option<i32>>) -> Option<i32> {
     match v {
-        Some(r @ Some(1..=10)) => r,
+        Some(r @ Some(1..=10) | r @ Some(101..)) => r,
         _ => None,
     }
 }
