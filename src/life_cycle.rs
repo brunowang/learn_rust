d11755f48e8442237d4ba17801c73a7ec2ede957
brunowang <625653507@qq.com>
2024-01-04 03:00:46 +0800

    test drop() method before life cycle finish

---
+++ src/life_cycle.rs
@@ -0,0 +1,12 @@
+#[derive(Debug, Clone, Copy)]
+struct Num(i32);
+
+fn main() {
+    let v: Vec<Num> = vec![Num(1),Num(2),Num(3),Num(4),Num(5)];
+    {
+        let center = v[2];
+        println!("{:?}", center);
+        // drop(center);
+    }
+    println!("{:?}", v);
+}
