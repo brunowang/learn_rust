ab28d1c4d573e9c57bc65caad94996f137c628da
brunowang <625653507@qq.com>
2024-04-06 14:51:27 +0800

    practice multi trait impl

--- src/main2.rs
+++ src/main2.rs
@@ -17,13 +17,18 @@ fn show_prods_price_sum(b: &[&Book], p: &[&Phone]) {
     println!("The product's price sum is: {}", sum)
 }
 
+fn show_detail<T>(p: &T) where T: Prods + Stock {
+    println!("book's price: {:?}", p.get_discount_price(1.0));
+    println!("book's stock: {:?}", p.get_stock());
+}
+
 fn main() {
     let book: Book = Prods::new(101, 25.0);
     let book2: Book = Prods::new(102, 30.0);
     let phone: Phone = Prods::new(201, 2799.0);
     let phone2: Phone = Prods::new(202, 1399.0);
     show_prods_price_sum(&[&book, &book2], &[&phone, &phone2]);
-    println!("book's stock: {:?}, {:?}", book.get_stock(), book2.get_stock());
+    show_detail(&book);
     println!("{:?}, {:?}", book, book2);
     println!("{:?}, {:?}", phone, phone2);
 }
