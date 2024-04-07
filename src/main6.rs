282958076f3157d7ae2ff49f2f9b68d35fd283de
brunowang <625653507@qq.com>
2024-04-08 00:05:06 +0800

    practice tokio tcp server

---
+++ src/main6.rs
@@ -0,0 +1,22 @@
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpListener;
+use tokio::time::{sleep, timeout, Duration};
+
+async fn job() -> String {
+    String::from("job executed")
+}
+
+#[tokio::main]
+async fn main() {
+    sleep(Duration::from_secs(1)).await;
+    println!("execute after sleeping");
+    let ret = timeout(Duration::from_secs(1), job()).await;
+    println!("timeout job {}", ret.unwrap());
+
+    let server = TcpListener::bind("0.0.0.0:80").await.unwrap();
+    loop {
+        let (mut stream, _) = server.accept().await.unwrap();
+        let rsp = String::from("http/1.1 200 OK\r\n\r\n rust server");
+        stream.write_all(rsp.as_bytes()).await.unwrap();
+    }
+}
