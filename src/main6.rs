280e3008a5dc6375819795aea634d03aafed7026
brunowang <625653507@qq.com>
2024-04-08 10:09:12 +0800

    practice tokio task

--- src/main6.rs
+++ src/main6.rs
@@ -1,5 +1,6 @@
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpListener;
+use tokio::task;
 use tokio::time::{sleep, timeout, Duration};
 
 async fn job() -> String {
@@ -16,7 +17,15 @@ async fn main() {
     let server = TcpListener::bind("0.0.0.0:80").await.unwrap();
     loop {
         let (mut stream, _) = server.accept().await.unwrap();
-        let rsp = String::from("http/1.1 200 OK\r\n\r\n rust server");
-        stream.write_all(rsp.as_bytes()).await.unwrap();
+        task::spawn(async move {
+            let mut rsp = String::from("http/1.1 200 OK\r\n\r\n rust server\t");
+            let ret = task::spawn_blocking(|| "blocking").await;
+            if let Ok(s) = ret {
+                rsp.push_str(s);
+            }
+            stream.write_all(rsp.as_bytes()).await.unwrap();
+        })
+        .await
+        .unwrap();
     }
 }
