af000085d694cd04a98676b5f50f302d9dd24a91
brunowang <625653507@qq.com>
2024-04-14 18:01:46 +0800

    practice Copy && Clone

--- src/main7.rs
+++ src/main7.rs
@@ -1,11 +1,27 @@
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug)]
 struct User {
-    id: i32
+    id: i32,
+    name: String,
+    age: i32,
+}
+
+impl Clone for User {
+    fn clone(&self) -> Self {
+        User {
+            id: self.id,
+            name: self.name.clone(),
+            age: self.age,
+        }
+    }
 }
 
 fn main() {
-    let a = User{id: 123};
-    let b = a;
+    let a = User {
+        id: 123,
+        name: String::from("Bruno"),
+        age: 33,
+    };
+    let b = a.clone();
     println!("a.id={}, b.id={}", a.id, b.id);
     println!("a={:?}, b={:?}", a, b);
 }
